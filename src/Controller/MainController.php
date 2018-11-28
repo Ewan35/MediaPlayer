afc9d0d10b3048f45a2ea25cd834bf9eabaabb73
Ewan35 <35138617+Ewan35@users.noreply.github.com>
2018-11-28 10:34:05 +0100

    puh

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * Class MainController
@@ -18,5 +22,60 @@ class MainController extends Controller
     public function home()
     {
         return $this->render("main/home.html.twig");
+    }
+
+    /**
+     * @Route("/register", name="main_register")
+     */
+    public function registration(Request $request,
+                                 UserPasswordEncoderInterface $encoder,
+                                 EntityManagerInterface $em){
+
+        $user = new Utilisateur();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //crypter le mot de passe
+            $pass = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($pass);
+            $user->setRoles(['ROLE_USER']);
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('main_index');
+
+        }
+
+        return $this->render('main/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('main/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){
+
     }
 }
